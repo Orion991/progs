0d0ffde118daa6d9792afe8fbd47546fc62c265a
TommyNe <thomas.nehrenberg@me.com>
2021-01-28 19:51:44 +0100

    Dashboard new Menue entry

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -3,9 +3,12 @@
 
 <main class="flex-shrink-0">
     <div class="container">
+        <h1 class="h1">Dashboard</h1>
         <br />
         <br />
-     <a href="logout">logout</a>
+        <a href="logout">logout</a><br/>
+        <a href="posts-admin">Posts Verwalten</a>
+        <a href="posts-new">Neue Einträge</a>
     </div>
 </main>
 <?php include __DIR__ . "/../layout/footer.php"; ?>
